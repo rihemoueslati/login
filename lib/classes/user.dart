9cb6723469baa89858eb16815ebe8e896991373a
rihemoueslati <89087637+rihemoueslati@users.noreply.github.com>
2022-04-15 04:09:41 +0200

    Add files via upload

---
+++ lib/classes/user.dart
@@ -0,0 +1,10 @@
+class User {
+  late String firstname;
+  late String lastname ;
+  late String email;
+  late String password;
+  late String numtel;
+  User(this.password,this.email,this.firstname,this.lastname, this.numtel);
+
+
+}
